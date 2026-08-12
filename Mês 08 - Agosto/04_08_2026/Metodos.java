8322fe2a077f94990c2abbf1c06e2152f2bb77b1
diogobennertz <diogo.bennertz@gmail.com>
2026-08-11 21:28:04 -0300

    .

---
+++ Mês 08 - Agosto/04_08_2026/Metodos.java
@@ -0,0 +1,14 @@
+public class Metodos {
+    public static void main(String[] args) {
+
+       chamaProfessor();
+        chamaAlunos();
+
+    }
+    public static void chamaProfessor(){
+        System.out.println("O Professor se chama Henrique");
+    }
+    public static void chamaAlunos(){
+        System.out.println("O Aluno se chama Diogo");
+    }
+}
