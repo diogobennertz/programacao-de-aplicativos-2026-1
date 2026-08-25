7a8d4df17082ecd6bd14b943ba395a2c1b4333de
diogobennertz <diogo.bennertz@gmail.com>
2026-08-25 20:55:26 -0300

    Create numeroNove.java

---
+++ Exercicios(não farei uma pasta pra cada dia)/Metodos/numeroNove.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class numeroNove {
+    //9)
+    //Faça um método booleano que retorne true se o número passado como parâmetro
+    //seja par.
+    //Retorne false caso seja ímpar
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.println("Informe o numero: ");
+        double n1 = leitor.nextDouble();
+        System.out.println(verdadeOuNao(n1));
+    }
+    public static boolean verdadeOuNao(double n1){
+        if (n1%2==0){
+            return true;
+        }
+        return false;
+    }
+}
