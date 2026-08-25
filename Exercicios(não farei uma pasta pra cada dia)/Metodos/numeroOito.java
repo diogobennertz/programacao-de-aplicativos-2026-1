33f3fe569fea495a7b90a71add66042d0203e815
diogobennertz <diogo.bennertz@gmail.com>
2026-08-25 19:47:34 -0300

    .

--- Exercicios(não farei uma pasta pra cada dia)/Metodos/numeroOito.java
+++ Exercicios(não farei uma pasta pra cada dia)/Metodos/numeroOito.java
@@ -15,7 +15,11 @@ public class numeroOito {
         System.out.println("a media é: " + mediaNotas(n1,n2,n3));
     }
     public static double mediaNotas(double n1,double n2,double n3){
+
+       if (n1<0 || n2<0 || n3<0 || n1>10 || n2>10 || n3>10) {
+         return 0;
+       }
        double media = (n1+n2+n3)/3;
-        return media;
+       return media;
     }
 }
