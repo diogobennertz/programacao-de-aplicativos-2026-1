0c3fd0fef108e683911e3cb6c050aef0b7cd4065
diogobennertz <diogo.bennertz@gmail.com>
2026-08-26 20:55:45 -0300

    Update ClassePrincipal.java

--- Exercicios Poo/ex126082026/src/ClassePrincipal.java
+++ Exercicios Poo/ex126082026/src/ClassePrincipal.java
@@ -7,6 +7,16 @@ public class ClassePrincipal {
         vPalio.modelo = "Fire";
         vPalio.placa = "MGM-2495";
         vPalio.preco = 26.000;
-        System.out.println(vPalio);
+        System.out.println(vPalio.marca);
+
+        Veiculo vCivic = new Veiculo();
+        vCivic.marca = "Honda";
+        vCivic.modelo = "Civic";
+        vCivic.placa = "MGN2344";
+        vCivic.ano = 2015;
+        vCivic.preco = 70000;
+        System.out.println(vCivic.preco);
+
+
     }
 }
