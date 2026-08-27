4a6eabf54cd0d008c4f0c3b010f5c2713188979c
diogobennertz <diogo.bennertz@gmail.com>
2026-08-26 21:29:43 -0300

    .

--- Exercicios Poo/ex126082026/src/ClassePrincipal.java
+++ Exercicios Poo/ex126082026/src/ClassePrincipal.java
@@ -1,22 +1,24 @@
-public class ClassePrincipal {
+public class classePrincipal {
     public static void main(String[] args) {
+        //1)
+        //Em uma classe denominada Usuario, deseja-se manter o nome, sexo, data de nascimento e
+        //estado civil desse usuário.
+        //O sexo deve ser definido como um caractere.
+        //A data deve ser definida como String.
+        //Crie 2 objetos de Usuario, atribua valores a esses objetos e mostre as informações na tela
 
-        Veiculo vPalio = new Veiculo();
-        vPalio.marca = "Fiat";
-        vPalio.ano = 2009;
-        vPalio.modelo = "Fire";
-        vPalio.placa = "MGM-2495";
-        vPalio.preco = 26.000;
-        System.out.println(vPalio.marca);
+        Usuario uDiogo = new Usuario();
+        uDiogo.estadoCivil = "Solteiro";
+        uDiogo.data = "02/04/2003";
+        uDiogo.sexo = 'M';
+        uDiogo.nome = "Diogo";
+        System.out.println(uDiogo.data +" " + uDiogo.estadoCivil + " " + uDiogo.sexo + " " + uDiogo.nome);
 
-        Veiculo vCivic = new Veiculo();
-        vCivic.marca = "Honda";
-        vCivic.modelo = "Civic";
-        vCivic.placa = "MGN2344";
-        vCivic.ano = 2015;
-        vCivic.preco = 70000;
-        System.out.println(vCivic.preco);
-
-
+        Usuario uEduardo = new Usuario();
+        uEduardo.estadoCivil = "Solteiro";
+        uEduardo.data = "02/04/2004";
+        uEduardo.sexo = 'F';
+        uEduardo.nome = "Eduardo";
+        System.out.println(uEduardo.data +" " + uEduardo.estadoCivil + " " + uEduardo.sexo + " " + uEduardo.nome);
     }
 }
