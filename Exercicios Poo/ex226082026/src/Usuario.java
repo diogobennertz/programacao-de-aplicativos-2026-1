28c60d616d587d3460d9df82e13ae9cda6125053
diogobennertz <diogo.bennertz@gmail.com>
2026-08-26 21:17:52 -0300

    .

---
+++ Exercicios Poo/ex226082026/src/Usuario.java
@@ -0,0 +1,13 @@
+public class Usuario {
+    //1)
+    //Em uma classe denominada Usuario, deseja-se manter o nome, sexo, data de nascimento e
+    //estado civil desse usuário.
+    //O sexo deve ser definido como um caractere.
+    //A data deve ser definida como String.
+    //Crie 2 objetos de Usuario, atribua valores a esses objetos e mostre as informações na tela
+    String nome;
+    String estadoCivil;
+    char sexo;
+    String data;
+
+}
