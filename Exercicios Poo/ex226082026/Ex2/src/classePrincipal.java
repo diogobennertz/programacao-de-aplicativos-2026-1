fe3bdb41ec9b4270bd4bcae071f306b0937ea354
Diogo Bennertz <diogo.bennertz@gmail.com>
2026-09-01 19:05:58 -0300

    .

--- Exercicios Poo/ex226082026/Ex2/src/classePrincipal.java
+++ Exercicios Poo/ex226082026/Ex2/src/classePrincipal.java
@@ -1,3 +1,45 @@
 public class classePrincipal {
 
+    //Crie 3 objetos de Mercado chamados:
+    //unidadeDeBlumenau
+    //unidadeDeJoinville
+    //unidadeDeFlorianopolis
+    //Atribua valores a esses objetos e mostre suas informações
+    public static void main(String[] args) {
+
+        mercado unidadeDeBlumenau = new mercado();
+        mercado unidadeDeJoinville = new mercado();
+        mercado unidadeDeFlorianopolis = new mercado();
+        unidadeDeBlumenau.nome = "Mercado de Blumenau";
+        unidadeDeFlorianopolis.nome = "Mercado de Florianópolis";
+        unidadeDeJoinville.nome = "Mercado de Joinville";
+        unidadeDeBlumenau.numLaranjas = 5;
+        unidadeDeFlorianopolis.numLaranjas = 6;
+        unidadeDeJoinville.numLaranjas = 7;
+        unidadeDeBlumenau.precoLaranjas = 3.50;
+        unidadeDeJoinville.precoLaranjas = 3.75;
+        unidadeDeFlorianopolis.precoLaranjas = 4.00;
+        unidadeDeBlumenau.precoMacas = 2.50;
+        unidadeDeFlorianopolis.precoMacas = 3.50;
+        unidadeDeJoinville.precoMacas = 2.75;
+        unidadeDeBlumenau.numMacas = 7;
+        unidadeDeFlorianopolis.numMacas = 6;
+        unidadeDeJoinville.numMacas = 5;
+
+        System.out.println("Unidade: " + unidadeDeBlumenau.nome);
+        System.out.println("Laranjas - Qtd: " + unidadeDeBlumenau.numLaranjas + " | Preço: R$" + unidadeDeBlumenau.precoLaranjas);
+        System.out.println("Maçãs    - Qtd: " + unidadeDeBlumenau.numMacas + " | Preço: R$" + unidadeDeBlumenau.precoMacas);
+        System.out.println("----------------------------------------");
+
+        System.out.println("Unidade: " + unidadeDeFlorianopolis.nome);
+        System.out.println("Laranjas - Qtd: " + unidadeDeFlorianopolis.numLaranjas + " | Preço: R$" + unidadeDeFlorianopolis.precoLaranjas);
+        System.out.println("Maçãs    - Qtd: " + unidadeDeFlorianopolis.numMacas + " | Preço: R$" + unidadeDeFlorianopolis.precoMacas);
+        System.out.println("----------------------------------------");
+
+        System.out.println("Unidade: " + unidadeDeJoinville.nome);
+        System.out.println("Laranjas - Qtd: " + unidadeDeJoinville.numLaranjas + " | Preço: R$" + unidadeDeJoinville.precoLaranjas);
+        System.out.println("Maçãs    - Qtd: " + unidadeDeJoinville.numMacas + " | Preço: R$" + unidadeDeJoinville.precoMacas);
+
+    }
+
 }
---
+++ Exercicios Poo/ex226082026/Ex2/src/mercado.java
@@ -0,0 +1,14 @@
+public class mercado {
+    //Crie uma classe denominada Mercado.
+    //Essa classe terá 5 atributos, as informações que deverão ser guardadas são:
+    //• Nome do mercado
+    //• Número de maçãs vendidas por ano.
+    //• Preço de venda das maçãs.
+    //• Número de laranjas vendidas por ano.
+    //• Preço de venda das laranjas.
+    String nome;
+    int numMacas;
+    double precoMacas;
+    int numLaranjas;
+    double precoLaranjas;
+}
