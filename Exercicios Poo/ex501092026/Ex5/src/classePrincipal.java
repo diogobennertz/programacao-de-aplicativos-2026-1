3ae69e8066ae72e9017c7569becf2b4a712c1563
Diogo Bennertz <diogo.bennertz@gmail.com>
2026-09-01 21:25:36 -0300

    .

---
+++ Exercicios Poo/ex501092026/Ex5/src/classePrincipal.java
@@ -0,0 +1,16 @@
+//5)
+//Crie uma classe Funcionário que terá como atributos:
+//• Identificação
+//• Nome
+//• Sobrenome
+//• Salário (mensal)
+//Crie métodos para:
+//Obter o salário anual do funcionário
+//Obter o nome completo do funcionário
+//Modificar o salário. O parâmetro do método deve ser o percentual de aumento
+
+public class classePrincipal {
+    public static void main(String[] args) {
+
+    }
+}
